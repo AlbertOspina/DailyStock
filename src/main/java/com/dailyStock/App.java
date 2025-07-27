4b125e4b6f3253bca72219a62669702fbea0aa75
AlbertoOspina-Babel <alberto.ospina@babelgroup.com>
2025-07-28 00:39:21 +0200

    Correcciones de Sonar y ajustes

--- src/main/java/com/dailyStock/App.java
+++ src/main/java/com/dailyStock/App.java
@@ -2,55 +2,41 @@ package com.dailyStock;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Logger;
 
 public class App {
 
-    public static final String urlTiingo = "https://api.tiingo.com/tiingo/daily/";
-    public static final String token = "?token=fc9a0e23dde6d1fd1757a6c222057af4b6948d21";
-;
-    public static void main(String[] args) {
-        System.out.println("Hola " + args[0]);
-        String query = args[1] + "/prices";
-        StringBuffer content = new StringBuffer();
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode Jnode;
+    public static final String URL_TIINGO = "https://api.tiingo.com/tiingo/daily/";
+    public static final String TOKEN = "?token=fc9a0e23dde6d1fd1757a6c222057af4b6948d21";
+    static Logger logger = Logger.getLogger(App.class.getName());
 
+    public static void main(String[] args) {
+        logger.info("Hola " + args[0]);
+        String query = args[1] + "/prices";
+        String resultado = "";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jNode = null;
 
         try {
-            URL url = new URL(urlTiingo + query + token);
-            System.out.println(url.toString());
+            URL url = new URL(URL_TIINGO + query + TOKEN);
+            logger.info(url.toString());
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("Content-Type", "application/json");
-
-            //System.out.println(con.getResponseCode());
-            /*BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
-            String inputLine;
-
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();*/
-
             InputStream response = con.getInputStream();
+            jNode = mapper.readTree(response);
 
-            Jnode = mapper.readTree(response);
+            jNode = jNode.get(0);
+            jNode = jNode.get("close");
+            resultado = jNode.asText();
 
-
-        } catch (MalformedURLException e){
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e){
+            resultado = e.toString();
         }
 
-        System.out.println(Jnode);
-
-
+        logger.info(resultado);
     }
 }
