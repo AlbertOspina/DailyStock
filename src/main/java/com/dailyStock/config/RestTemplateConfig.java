ae2ed0419e4295ca3e29bf9f2175e823d0f1a6ce
AlbertoOspina-Babel <alberto.ospina@babelgroup.com>
2025-08-06 03:14:32 +0200

    Ajustes para deshabilitar validacion de cert

--- src/main/java/com/dailyStock/config/RestTemplateConfig.java
+++ src/main/java/com/dailyStock/config/RestTemplateConfig.java
@@ -1,17 +1,50 @@
 package com.dailyStock.config;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        // Spring Boot configura automáticamente el RestTemplateBuilder por ti.
-        // Aquí podrías añadir configuraciones base si lo necesitaras.
-        return builder.build();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) throws NoSuchAlgorithmException, KeyManagementException {
+
+        // Peligro: Este TrustManager no hace nada, aceptando cualquier certificado.
+        final SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, new X509TrustManager[]{new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {}
+            @Override
+            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {}
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        }}, null);
+
+        // Cliente HTTP que usa nuestro SSLContext "inseguro" y deshabilita la verificación del hostname.
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLContext(sslContext)
+                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE) // Deshabilita la validación del nombre del host
+                .build();
+
+        HttpComponentsClientHttpRequestFactory customRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        customRequestFactory.setHttpClient(httpClient);
+
+        // Construye el RestTemplate con nuestra factory "insegura"
+        return builder.requestFactory(() -> customRequestFactory).build();
     }
 }
--- src/main/java/com/dailyStock/repository/TiingoRepository.java
+++ src/main/java/com/dailyStock/repository/TiingoRepository.java
@@ -1,6 +1,7 @@
 package com.dailyStock.repository;
 
 import com.dailyStock.model.StockDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
@@ -8,10 +9,13 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 @Repository
+@Slf4j
 public class TiingoRepository {
     public static final String URL_TIINGO = "https://api.tiingo.com/tiingo/daily/";
+    public static final String STOCK_PATH = "/prices";
     public static final String TOKEN = "?token=fc9a0e23dde6d1fd1757a6c222057af4b6948d21";
 
     private final RestTemplate restTemplate;
@@ -22,14 +26,10 @@ public class TiingoRepository {
     }
 
     public List<StockDTO> getStockData(String tiker) {
-        String url = URL_TIINGO+tiker+"/price"+TOKEN;
-
-        // Usamos getForObject para hacer la petición GET.
-        // ¡Ojo al truco! Como la respuesta es un arreglo JSON, le pedimos a RestTemplate
-        // que lo mapee a un ARREGLO de PriceDataDTO (PriceDataDTO[].class).
+        String url = URL_TIINGO+tiker+STOCK_PATH+TOKEN;
+        log.info(url);
         StockDTO[] response = restTemplate.getForObject(url, StockDTO[].class);
 
-        // Luego, convertimos el arreglo a una Lista para que sea más fácil de manejar.
         return Arrays.asList(Objects.requireNonNull(response));
     }
 }
